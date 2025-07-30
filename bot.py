da3ab8d7a13a1d9ad6312f6b83f5928734254be5
kaungyelu <kaung2016.ye@gmail.com>
2025-07-30 16:39:29 +0630

    Update bot.py

--- bot.py
+++ bot.py
@@ -387,7 +387,7 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
                     total_amount += amount
 
         if not all_bets:
-            await update.message.reply_text("⚠️ အချက်အလက်များကိုစစ်ဆေးပါ\nဥပမာ: 12-1000,12/34-1000 \n 12r1000,12r1000-500")
+            await update.message.reply_text("⚠️ အချက်အလက်များကိုစစ်ဆေးပါ\nဥပမာ: 12-1000,12/34-1000 \n 12r1000,12r1000-500\nအပူး ပါဝါ အပါ")
             return
 
         # Update data stores
